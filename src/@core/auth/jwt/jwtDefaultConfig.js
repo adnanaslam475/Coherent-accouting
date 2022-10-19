5833e737e0e5febe80c8179617ce66fa2acd8a2a
Muzammil Sharif <muzammilsharifmms@gmail.com>
2022-10-19 18:48:26 +0500

    initial commit with theme setup

---
+++ src/@core/auth/jwt/jwtDefaultConfig.js
@@ -0,0 +1,17 @@
+export default {
+  // Endpoints
+  loginEndpoint: '/jwt/login',
+  clientToken: "/userauth/oauth/token",
+  // registerEndpoint: '/jwt/register',
+  registerEndpoint: '/account/api/user/create',
+  refreshEndpoint: '/jwt/refresh-token',
+  logoutEndpoint: '/jwt/logout',
+
+  // This will be prefixed in authorization header with token
+  // e.g. Authorization: Bearer <token>
+  tokenType: 'Bearer',
+
+  // Value of this property will be used as key to store JWT token in storage
+  storageTokenKeyName: 'accessToken',
+  storageRefreshTokenKeyName: 'refreshToken',
+}
---
+++ src/@core/auth/jwt/jwtService.js
@@ -0,0 +1,138 @@
+import jwtDefaultConfig from './jwtDefaultConfig'
+import axios from 'axios'
+var qs = require('qs');
+export default class JwtService {
+  // Will be used by this service for making API calls
+  axiosIns = null
+
+  axiosIns1 = axios.create({
+    // You can add your headers here
+    // ================================
+    baseURL: 'http://167.86.93.80:8899',
+    // timeout: 1000,
+    headers: { 
+      'Content-Type': 'application/x-www-form-urlencoded', 
+      'Accept': 'application/json', 
+      'Authorization': 'Basic cmVnaXN0ZXItYXBwOmFjbWVzZWNyZXQ='
+    }
+  })
+
+  // jwtConfig <= Will be used by this service
+  jwtConfig = { ...jwtDefaultConfig }
+
+  // For Refreshing Token
+  isAlreadyFetchingAccessToken = false
+
+  // For Refreshing Token
+  subscribers = []
+
+  constructor(axiosIns, jwtOverrideConfig) {
+    this.axiosIns = axiosIns
+    this.jwtConfig = { ...this.jwtConfig, ...jwtOverrideConfig }
+
+    // Request Interceptor
+    this.axiosIns.interceptors.request.use(
+      config => {
+        // Get token from localStorage
+        const accessToken = this.getToken()
+
+        // If token is present add it to request's Authorization Header
+        if (accessToken) {
+          // eslint-disable-next-line no-param-reassign
+          config.headers.Authorization = `${this.jwtConfig.tokenType} ${accessToken}`
+        }
+        return config
+      },
+      error => Promise.reject(error),
+    )
+
+    // Add request/response interceptor
+    this.axiosIns.interceptors.response.use(
+      response => response,
+      error => {
+        // const { config, response: { status } } = error
+        const { config, response } = error
+        const originalRequest = config
+
+        // if (status === 401) {
+        if (response && response.status === 401) {
+          if (!this.isAlreadyFetchingAccessToken) {
+            this.isAlreadyFetchingAccessToken = true
+            this.refreshToken().then(r => {
+              this.isAlreadyFetchingAccessToken = false
+
+              // Update accessToken in localStorage
+              this.setToken(r.data.accessToken)
+              this.setRefreshToken(r.data.refreshToken)
+
+              this.onAccessTokenFetched(r.data.accessToken)
+            })
+          }
+          const retryOriginalRequest = new Promise(resolve => {
+            this.addSubscriber(accessToken => {
+              // Make sure to assign accessToken according to your response.
+              // Check: https://pixinvent.ticksy.com/ticket/2413870
+              // Change Authorization header
+              originalRequest.headers.Authorization = `${this.jwtConfig.tokenType} ${accessToken}`
+              resolve(this.axiosIns(originalRequest))
+            })
+          })
+          return retryOriginalRequest
+        }
+        return Promise.reject(error)
+      },
+    )
+  }
+
+  onAccessTokenFetched(accessToken) {
+    this.subscribers = this.subscribers.filter(callback => callback(accessToken))
+  }
+
+  addSubscriber(callback) {
+    this.subscribers.push(callback)
+  }
+
+  getToken() {
+    return localStorage.getItem(this.jwtConfig.storageTokenKeyName)
+  }
+
+  getRefreshToken() {
+    return localStorage.getItem(this.jwtConfig.storageRefreshTokenKeyName)
+  }
+
+  setToken(value) {
+    localStorage.setItem(this.jwtConfig.storageTokenKeyName, value)
+  }
+
+  setRefreshToken(value) {
+    localStorage.setItem(this.jwtConfig.storageRefreshTokenKeyName, value)
+  }
+
+  login(...args) {
+    return this.axiosIns.post(this.jwtConfig.loginEndpoint, ...args)
+  }
+
+  clientToken(){
+    const data = qs.stringify({
+      'grant_type': 'client_credentials' 
+    })
+    return this.axiosIns1.post(this.jwtConfig.clientToken, data)
+  }
+
+  register(token,...args) {
+    const headers = {
+      'Content-Type': 'application/json', 
+      'Authorization': `Bearer ${token}`,
+      'Accept': 'application/json'
+    }
+    return this.axiosIns.post(this.jwtConfig.registerEndpoint, ...args, {
+      headers: headers
+    })
+  }
+
+  refreshToken() {
+    return this.axiosIns.post(this.jwtConfig.refreshEndpoint, {
+      refreshToken: this.getRefreshToken(),
+    })
+  }
+}
