eee23603d6649b6e9c9327923a8d8cb3ff28bbac
Muhamad Umer <umerdev228@gmail.com>
2022-11-02 17:07:48 +0500

    axios token interceptor added

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -12,6 +12,21 @@ const axiosIns = axios.create({
   // headers: {'X-Custom-Header': 'foobar'}
 })
 
+axiosIns.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('access_token')
+    if (token) {
+      // eslint-disable-next-line no-param-reassign
+      config.headers.Authorization = `Bearer ${token}`
+      // eslint-disable-next-line no-param-reassign
+      config.headers['Content-Type'] = 'application/json'
+    } else {
+      // Do something... Usually logout user.
+    }
+    return config
+  },
+)
+
 Vue.prototype.$http = axiosIns
 
 export default axiosIns
