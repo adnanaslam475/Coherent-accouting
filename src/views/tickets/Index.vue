a09f83066431940b992fc19ecbb629eb0efd9a0c
Muhamad Umer <umerdev228@gmail.com>
2022-11-15 08:50:49 +0500

    ticket fixes

--- src/views/tickets/Index.vue
+++ src/views/tickets/Index.vue
@@ -1,46 +1,6 @@
 <template>
   <b-card>
     <div class="row">
-      <!-- <input data-v-9a6e255c="" type="text" placeholder="Search..." class="d-inline-block mr-1 form-control col-4" style="margin-left: 15px" /> -->
-      <div class="input-group col-4 abc">
-        <input
-          data-v-15eba8c6=""
-          type="text"
-          placeholder="Search Product"
-          v-model="searchQuery"
-          class="search-product form-control "
-          id="__BVID__3198"
-          @keyup="searchCompanies()"
-        />
-        <div data-v-15eba8c6="" class="input-group-append">
-          <div data-v-15eba8c6="" class="input-group-text" style="height: 38px; cursor:pointer">
-            <!-- #7367f0 -->
-            <svg
-              data-v-15eba8c6=""
-              xmlns="http://www.w3.org/2000/svg"
-              width="14px"
-              height="14px"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="text-muted feather feather-search"
-            >
-              <circle data-v-15eba8c6="" cx="11" cy="11" r="8"></circle>
-              <line
-                data-v-15eba8c6=""
-                x1="21"
-                y1="21"
-                x2="16.65"
-                y2="16.65"
-              ></line>
-            </svg>
-          </div>
-        </div>
-        <!---->
-      </div>
       <b-button
         v-b-modal.ticket-create-modal
         variant="relief-primary"
@@ -54,18 +14,36 @@
       :items="items"
       responsive
       class="mb-0"
-    ></b-table>
-
+    >
+      <template style="text-align: center !important" #cell(action)="data">
+        <!-- Dropdown -->
+        <b-dropdown
+          variant="link"
+          toggle-class="p-0"
+          no-caret
+          :right="$store.state.appConfig.isRTL"
+        >
+          <template #button-content>
+            <feather-icon
+              icon="MoreVerticalIcon"
+              size="16"
+              class="align-middle text-body ml-1"
+            />
+          </template>
+          <b-dropdown-item @click="editTicket(data.item, data.item.id)">
+            <feather-icon icon="EditIcon" />
+            <span class="align-middle ml-50">Edit</span>
+          </b-dropdown-item>
+        </b-dropdown>
+        <!-- </b-button> -->
+      </template>
+    </b-table>
     <div class="mx-2 mb-2 mt-2">
       <b-row>
         <b-col
           cols="12"
           sm="6"
-          class="
-            d-flex
-            align-items-center
-            justify-content-center justify-content-sm-start
-          "
+          class="d-flex align-items-center justify-content-center justify-content-sm-start"
         >
           <!-- <span class="text-muted"
             >Showing {{ currentPage + 9 - 9 }} to {{ currentPage + 9 }} of
@@ -76,11 +54,7 @@
         <b-col
           cols="12"
           sm="6"
-          class="
-            d-flex
-            align-items-center
-            justify-content-center justify-content-sm-end
-          "
+          class="d-flex align-items-center justify-content-center justify-content-sm-end"
         >
           <b-pagination
             v-model="currentPage"
@@ -92,13 +66,20 @@
             prev-class="prev-item"
             next-class="next-item"
             prev-text
+            @input="getAllTickets"
           >
             <!--              @input="getNewRecord"-->
             <template #prev-text>
-              <feather-icon icon="ChevronLeftIcon" size="18"/>
+              <feather-icon
+                icon="ChevronLeftIcon"
+                size="18"
+              />
             </template>
             <template #next-text>
-              <feather-icon icon="ChevronRightIcon" size="18"/>
+              <feather-icon
+                icon="ChevronRightIcon"
+                size="18"
+              />
             </template>
           </b-pagination>
         </b-col>
@@ -110,8 +91,6 @@
       title="Add New Ticket"
       ok-title="Submit"
       cancel-variant="outline-secondary"
-      @show="resetModal"
-      @hidden="resetModal"
       @ok="handleOk"
     >
       <b-card-text>
@@ -136,8 +115,8 @@
               >
 
                 <b-form-textarea
-                  v-model="ticket.ticketContent"
                   id="textarea-default"
+                  v-model="ticket.ticketContent"
                   placeholder="Textarea"
                   rows="3"
                 />
@@ -147,7 +126,6 @@
         </b-form>
       </b-card-text>
     </b-modal>
-
   </b-card>
 </template>
 <script>
@@ -207,7 +185,9 @@ export default {
   },
   data() {
     return {
-      ticket: {},
+      ticket: {
+        title: 'hellow',
+      },
       fields: [
         {
           key: 'title',
@@ -229,6 +209,10 @@ export default {
           key: 'status',
           label: 'Status',
         },
+        {
+          key: 'action',
+          label: 'Action',
+        },
       ],
       items: [],
       currentPage: 1,
@@ -245,13 +229,11 @@ export default {
   methods: {
     // getting the list of all companies
     async getAllTickets() {
-      const data = await axios.get(`/account/api/ticket/list/${this.currentPage}/${this.perPage}?direction=desc&sortField=id`,)
-      console.log(data)
+      const data = await axios.get(`/account/api/ticket/list/${this.currentPage}/${this.perPage}?direction=desc&sortField=id`)
       if (data.data.elements !== '') {
         this.items = data.data.elements
         this.totalRecords = data.data.totalElements
         this.totalPages = Math.ceil(this.totalRecords / this.perPage)
-        // console.log(this.totalPages);
       }
     },
     async createTicket() {
@@ -281,6 +263,12 @@ export default {
         variant,
         solid: false,
       })
+    },
+    editTicket(ticket, id) {
+      this.ticket.title = ticket.title
+      this.ticket.ticketContent = ticket.ticketContent
+      console.log(ticket, id)
+      this.$bvModal.show('ticket-create-modal')
     },
   },
 }
