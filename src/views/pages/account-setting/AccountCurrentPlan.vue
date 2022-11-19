5725740e288e7e15ab881fc89a910f48332e7133
Muhamad Umer <umerdev228@gmail.com>
2022-11-19 07:21:34 +0500

    invoices added to company page

---
+++ src/views/pages/account-setting/AccountCurrentPlan.vue
@@ -0,0 +1,34 @@
+<template>
+  <b-card>
+    <b-card-title>Current Plan</b-card-title>
+    <b-row>
+      <b-col cols="6">
+
+      </b-col>
+    </b-row>
+  </b-card>
+</template>
+
+<script>
+import {
+  BCard,
+  BCardTitle,
+  BRow,
+  BCol,
+}
+from 'bootstrap-vue'
+
+export default {
+  name: 'AccountCurrentPlan',
+  components: {
+    BCard,
+    BCardTitle,
+    BRow,
+    BCol,
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
