f0f8d9d2a920aa3440295cb356baead286f90ebd
hotitemsfinder <hot.items.finder@gmail.com>
2022-12-14 20:09:36 +0200

    fixed refresh token

--- src/@core/auth/jwt/jwtService.js
+++ src/@core/auth/jwt/jwtService.js
@@ -69,37 +69,37 @@ export default class JwtService {
         // const { config, response } = error.config;
         const originalRequest = error.config;
 
-        // if (error.response && error.response.status === 401) {
-        //   if (error.response.data && error.response.data.error_description 
-        //     && error.response.data.error_description.includes('Access token expired:')) {
-        //     this.refreshToken().then((r) => {
-        //       // Update accessToken in localStorage
-        //       this.setToken(r.data.access_token);
-        //       this.setRefreshToken(r.data.refresh_token);
-        //       this.onAccessTokenFetched(r.data.access_token);
-        //     }).catch(error => {
-        //       if (error.response && error.response.status === 401) {
-        //         // && error.response.data && error.response.data.error_description 
-        //         // && error.response.data.error_description.includes('Invalid refresh token (expired):')) {
-        //         localStorage.removeItem(this.jwtConfig.storageTokenKeyName)
-        //         localStorage.removeItem(this.jwtConfig.storageRefreshTokenKeyName)
-        //         localStorage.removeItem('userData')
-        //         router.push({ name: 'login' })
-        //       }
-        //     });
+        if (error.response && error.response.status === 401) {
+          if (error.response.data && error.response.data.error_description 
+            && error.response.data.error_description.includes('Access token expired:')) {
+            this.refreshToken().then((r) => {
+              // Update accessToken in localStorage
+              this.setToken(r.data.access_token);
+              this.setRefreshToken(r.data.refresh_token);
+              this.onAccessTokenFetched(r.data.access_token);
+            }).catch(error => {
+              if (error.response && error.response.status === 401) {
+                // && error.response.data && error.response.data.error_description 
+                // && error.response.data.error_description.includes('Invalid refresh token (expired):')) {
+                localStorage.removeItem(this.jwtConfig.storageTokenKeyName)
+                localStorage.removeItem(this.jwtConfig.storageRefreshTokenKeyName)
+                localStorage.removeItem('userData')
+                router.push({ name: 'login' })
+              }
+            });
 
-        //   }
-        //   const retryOriginalRequest = new Promise((resolve) => {
-        //     this.addSubscriber((accessToken) => {
-        //       // Make sure to assign accessToken according to your response.
-        //       // Check: https://pixinvent.ticksy.com/ticket/2413870
-        //       // Change Authorization header
-        //       originalRequest.headers.Authorization = `${this.jwtConfig.tokenType} ${accessToken}`;
-        //       resolve(this.axiosIns(originalRequest));
-        //     });
-        //   });
-        //   return retryOriginalRequest;
-        // }
+          }
+          const retryOriginalRequest = new Promise((resolve) => {
+            this.addSubscriber((accessToken) => {
+              // Make sure to assign accessToken according to your response.
+              // Check: https://pixinvent.ticksy.com/ticket/2413870
+              // Change Authorization header
+              originalRequest.headers.Authorization = `${this.jwtConfig.tokenType} ${accessToken}`;
+              resolve(this.axiosIns(originalRequest));
+            });
+          });
+          return retryOriginalRequest;
+        }
         return Promise.reject(error);
       }
     );
