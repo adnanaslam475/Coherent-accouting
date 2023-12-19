0b0979f6bf94f893e34ac8a990a6dae201ad4e59
SafiaOmar <ambitiousoutlook@gmail.com>
2023-12-19 22:40:11 +0500

    Code refactoring, naming conventions fixes

--- src/encryptionUtil.js
+++ src/encryptionUtil.js
@@ -1,49 +1,49 @@
-const crypto = require("crypto");
+const crypto = require('crypto')
 
 class EncryptionUtil {
   constructor(key, initVector) {
-    this.key = key || "1234567812345678"; // Default key
-    this.initVector = initVector || "1234567812345678"; // Default IV
-    this.algo = "aes-256-cbc";
+    this.key = key || '1234567812345678' // Default key
+    this.initVector = initVector || '1234567812345678' // Default IV
+    this.algo = 'aes-256-cbc'
   }
 
   encrypt(value) {
     try {
       if (value) {
-        const iv = Buffer.from(this.initVector, "utf8");
-        const key = Buffer.from(this.key, "utf8");
-        const cipher = crypto.createCipheriv(this.algo, key, iv);
-        let encrypted = cipher.update(value, "utf8", "base64");
-        encrypted += cipher.final("base64");
-        return encrypted;
+        const iv = Buffer.from(this.initVector, 'utf8')
+        const key = Buffer.from(this.key, 'utf8')
+        const cipher = crypto.createCipheriv(this.algo, key, iv)
+        let encrypted = cipher.update(value, 'utf8', 'base64')
+        encrypted += cipher.final('base64')
+        return encrypted
       }
     } catch (ex) {
-      console.error(`Exception during encryption with error: ${ex}`);
+      console.error(`Exception during encryption with error: ${ex}`)
     }
-    return null;
+    return null
   }
 
   decrypt(encrypted) {
     try {
       if (encrypted) {
-        const iv = Buffer.from(this.initVector, "utf8");
-        const key = Buffer.from(this.key, "utf8");
-        const decipher = crypto.createDecipheriv(this.algo, key, iv);
-        let decrypted = decipher.update(encrypted, "base64", "utf8");
-        decrypted += decipher.final("utf8");
-        return decrypted;
+        const iv = Buffer.from(this.initVector, 'utf8')
+        const key = Buffer.from(this.key, 'utf8')
+        const decipher = crypto.createDecipheriv(this.algo, key, iv)
+        let decrypted = decipher.update(encrypted, 'base64', 'utf8')
+        decrypted += decipher.final('utf8')
+        return decrypted
       }
     } catch (ex) {
-      console.error(`Exception during decryption with error: ${ex}`);
+      console.error(`Exception during decryption with error: ${ex}`)
     }
-    return null;
+    return null
   }
 }
 
 export default {
   install(Vue, options) {
-    const encryptionUtil = new EncryptionUtil(options.key, options.initVector);
+    const encryptionUtil = new EncryptionUtil(options.key, options.initVector)
 
-    Vue.prototype.$encryptionUtil = encryptionUtil;
+    Vue.prototype.$encryptionUtil = encryptionUtil
   },
-};
+}
